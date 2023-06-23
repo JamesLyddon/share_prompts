e6f4e016da33253abd8e618dae3553fc00a453ae
JamesLyddon <59798913+JamesLyddon@users.noreply.github.com>
2023-06-23 14:48:19 +0100

    feat: Nav partially implemented

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed"
+
 const Home = () => {
   return (
     <section
@@ -12,7 +14,7 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool for the modern world to discover, create, and share creative prompts
       </p>
 
-      {/* Feed Component */}
+      <Feed />
     </section>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,40 @@
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"
+import { useState, useEffect } from "react"
+import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
+
 const Nav = () => {
+  const isUserLoggedIn = true
+  
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image className="object-contain" src="/assets/images/logo.svg" alt="Promptopia Logo" width={30} height={30}/>
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      {/* Desktop(larger than x-small) Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ?
+        (
+          <div className="flex gap-3 md:gap-5">
+            <Link className="black_btn" href="/create-prompt">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              {/* resume here */}
+            </button>
+          </div>
+        ) :
+        (
+          <></>
+        ) 
+      }
+      </div>
+    </nav>
   )
 }
 
